fa9b9241973611a0a2fa387533eae60c375b955a
sjllls <2012ji123@gmail.com>
2020-08-03 15:19:00 +0800

    aurora: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from tama-common
+-include device/sony/tama-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/sony/aurora
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := H8166,H8116,aurora,aurora_dual
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+# Kernel
+TARGET_KERNEL_CONFIG := tama_aurora_defconfig
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# Partitions
+# Reserve space for data encryption (44712771584-16384)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 44712755200
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Treble
+BOARD_VNDK_RUNTIME_DISABLE := true
+
+# Inherit from the proprietary version
+-include vendor/sony/aurora/BoardConfigVendor.mk
